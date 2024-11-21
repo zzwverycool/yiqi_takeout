8cad5846dfe85b21450c6057da2a20351f3dfbb4
zzwverycool <21051735@hdu.edu.cn>
2024-11-21 15:58:57 +0800

    商家外卖初始代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
+import com.sun.org.apache.bcel.internal.generic.RETURN;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +28,16 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //处理重复sql异常
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String[] split=message.split("");
+            String username = split[2];
+            String msg = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
